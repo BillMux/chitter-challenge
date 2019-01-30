846752aa4644aa8ad04b9dccdd4ee78bae6c5ecf
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-30 13:37:39 +0000

    Add before blocks

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 feature 'index page' do
-  scenario 'contains a header' do
+  before(:each) do
     visit '/'
-    expect(page).to have_content 'Chitter'
+  end
+
+  scenario 'contains a header' do
+    expect(page).to have_content('Chitter')
   end
 
   scenario 'has link to peep page' do
-    visit '/'
     expect(page).to have_link('Peep!', href: '/peep')
   end
 end
--- spec/features/peep_spec.rb
+++ spec/features/peep_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
-feature 'index page' do
+feature 'peep page' do
+  before(:each) do
+    visit '/peep'
+  end
+
   scenario 'contains a header' do
     visit '/peep'
     expect(page).to have_content 'Your Peep:'
