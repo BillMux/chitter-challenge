0fd1b59ac9827387341b4326b41fc47482e3014a
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-30 13:49:30 +0000

    Test text field

--- spec/features/peep_spec.rb
+++ spec/features/peep_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 feature 'peep page' do
   before(:each) do
-    visit '/peep'
+    visit('/peep')
   end
 
   scenario 'contains a header' do
-    visit '/peep'
-    expect(page).to have_content 'Your Peep:'
+    expect(page).to have_content('Your Peep:')
+  end
+
+  scenario 'contains an empty text field' do
+    expect(page).to have_field('peep-text')
   end
 end
