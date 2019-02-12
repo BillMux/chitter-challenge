c347eb313ab44916dc546abb0ffba2f2b91e52a0
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-12 13:19:49 +0000

    Revert back to dev db

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,6 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'lib/peep'
 
+ENV['ENVIRONMENT'] = 'development'
 DataMapper.setup(:default, "postgres://localhost:5432/chitter_development")
 DataMapper::Model.raise_on_save_failure = true
 DataMapper.finalize
