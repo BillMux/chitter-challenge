bad177049d672ad9f7237674a773d59f73683e9f
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-13 14:33:26 +0000

    Call setup helper in rspec config block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'simplecov'
 require 'simplecov-console'
 require 'rspec'
 require_relative '../chitter_app'
+require_relative 'setup_database'
 
 ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'chitter_app.rb')
@@ -11,7 +12,13 @@ require File.join(File.dirname(__FILE__), '..', 'chitter_app.rb')
 Capybara.app = ChitterApp
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
- SimpleCov::Formatter::Console,
- SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    setup_database
+  end
+end
