5a18978cff18e249c833a9b886278f35679fc5ed
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-13 11:14:16 +0000

    Interpolate environment, fix db location

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,8 +2,8 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'lib/peep'
 
-ENV['ENVIRONMENT'] = 'development'
-DataMapper.setup(:default, "postgres://localhost:5432/chitter_development")
+env = ENV['ENVIRONMENT']
+DataMapper.setup(:default, "postgres://localhost:5432/chitter_#{env}")
 DataMapper::Model.raise_on_save_failure = true
 DataMapper.finalize
 DataMapper.auto_upgrade!
