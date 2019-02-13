4f49ecc2d223cdb893ad5a941f5d6905e4c0f0a7
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-13 13:45:43 +0000

    Hardcode db address

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,8 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'lib/peep'
 
-env = ENV['ENVIRONMENT']
-DataMapper.setup(:default, "postgres://localhost:5432/chitter_#{env}")
+DataMapper.setup(:default, "postgres://localhost:5432/chitter_development")
 DataMapper::Model.raise_on_save_failure = true
 DataMapper.finalize
 DataMapper.auto_upgrade!
